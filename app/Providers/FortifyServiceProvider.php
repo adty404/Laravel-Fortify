d166ac3aabc045fe023f5d5c71041a76ce3e34e5
Aditya Prasetyo <adty404@gmail.com>
2022-11-22 21:32:59 +0700

    notes

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -8,6 +8,7 @@ use App\Actions\Fortify\UpdateUserPassword;
 use App\Actions\Fortify\UpdateUserProfileInformation;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Fortify\Fortify;
@@ -56,14 +57,36 @@ class FortifyServiceProvider extends ServiceProvider
             return view('auth.verify-email');
         });
 
+        Fortify::authenticateUsing(function (Request $request) {
+            $user = \App\Models\User::where('username', $request->username)
+                ->orWhere('email', $request->username)
+                ->first();
+
+            if ($user && Hash::check($request->password, $user->password)) {
+                return $user;
+            }
+        });
+
         RateLimiter::for('login', function (Request $request) {
             $email = (string) $request->email;
 
-            return Limit::perMinute(5)->by($email.$request->ip());
+            return Limit::perMinute(5)->by($email . $request->ip());
         });
 
         RateLimiter::for('two-factor', function (Request $request) {
             return Limit::perMinute(5)->by($request->session()->get('login.id'));
         });
+
+
+        // composer require laravel/ui
+        // php artisan ui bootstrap --auth
+        // npm install && npm run dev
+        // remove Auth::routes(); from routes\web.php
+        // remove auth controllers from app\Http\Controllers\Auth
+
+        // composer require laravel/fortify
+        // php artisan vendor:publish --provider="Laravel\Fortify\FortifyServiceProvider"
+        // php artisan migrate
+        // \App\Providers\FortifyServiceProvider::class, // <--- Add this line to config\app.php
     }
 }
